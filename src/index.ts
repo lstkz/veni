c3b73425e000c356e5934136f8612407046f5cfe
Łukasz Sentkiewicz <sirmims@gmail.com>
2018-12-08 11:14:10 +0100

    export V as class instance
    Signed-off-by: Łukasz Sentkiewicz <sirmims@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,37 @@ import { BooleanSchema } from './BooleanSchema';
 import { DateSchema } from './DateSchema';
 import { OrSchema } from './OrSchema';
 
-export const V = {
-  any: () => new AnySchema(),
-  string: () => new StringSchema(),
-  object: () => new ObjectSchema(),
-  array: () => new ArraySchema(),
-  enum: () => new EnumSchema(),
-  number: () => new NumberSchema(),
-  boolean: () => new BooleanSchema(),
-  date: () => new DateSchema(),
-  or: () => new OrSchema(),
-};
+export class Veni {
+  any() {
+    return new AnySchema();
+  }
+  string() {
+    return new StringSchema();
+  }
+  object() {
+    return new ObjectSchema();
+  }
+  array() {
+    return new ArraySchema();
+  }
+  enum() {
+    return new EnumSchema();
+  }
+  number() {
+    return new NumberSchema();
+  }
+  boolean() {
+    return new BooleanSchema();
+  }
+  date() {
+    return new DateSchema();
+  }
+  or() {
+    return new OrSchema();
+  }
+}
+
+export const V = new Veni();
 
 export {
   StringSchema,
