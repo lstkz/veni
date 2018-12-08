0ba45397832ef071fffffce7b04710061e156db9
Łukasz Sentkiewicz <sirmims@gmail.com>
2018-12-08 12:21:47 +0100

    fix build
    Signed-off-by: Łukasz Sentkiewicz <sirmims@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 rm -rf ./dist
 mkdir dist
-yarn run tsc --declaration true --module esnext --outDir "./dist"
-# yarn run tsc --declaration true --declarationMap true --module commonjs --outDir "./dist"
+yarn run tsc --declaration true --module esnext --outDir "./dist/es"
+yarn run tsc --declaration true --module commonjs --outDir "./dist"
 cp package.json ./dist/package.json
